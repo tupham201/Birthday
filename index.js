d44ae9c1b3dcaf1fcbac4445799b7df182c29b90
mihow201 <phamtattu201@gmail.com>
2025-07-16 23:08:19 +0700

    lan2

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const birthdayText = document.getElementById("birthdayText");
 const birthdayMusic = document.getElementById("birthdayMusic");
 
 const texts = [
-  "Chúc bro tuổi mới full niềm vui, full máu, full thành công luôn 🥳🔥",
-  "Học kỳ tới auto điểm cao, học bổng bay vào tay 😎✨",
-  "Sinh nhật thì quẩy banh nóc, quà nhận không kịp đếm, deadline tự né ra cho bro chill tẹt ga 🎁🎉"
+  "Chúc em Dương tuổi mới thật nhiều niềm vui, năm sau đỗ Nguyễn Tất Thành 🥳🔥",
+  "Học kỳ tới auto điểm cao, đạt nhiều giải cao 😎✨",
+  "Sinh nhật thì quẩy banh nóc, quà nhận không kịp đếm, deadline tự né ra cho em chill tẹt ga 🎁🎉"
 ];
 
 giftBox.addEventListener('click', () => {
